6250cdc7d1f68be9929e0b84048eca5cc97aff1c
Joshua Carey <jdcarey128@gmail.com>
2021-03-10 15:49:45 -0700

    Get all unit tests to pass

--- functions.js
+++ functions.js
@@ -1,9 +1,27 @@
 // functions.js
-function addTwoNumbers() {
+const addTwoNumbers = (num1, num2) => {
+    return num1 + num2
+}
+
+const sayHello = (name) => {
+    if (name === 'Will') {
+        return `No more testing Will!`
+    } else {
+        return `Hi there ${name}!`
+    }
+}
+
+const buildCar = (color, type) => {
+    var car = {}
+    if (color || type) {
+        car.type = type
+        car.color = color
+    } 
+    return car
 }
 
 module.exports = {
     addTwoNumbers,
-    // sayHello,
-    // buildCar
+    sayHello,
+    buildCar
 }
